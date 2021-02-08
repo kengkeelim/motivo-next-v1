e352ac969ef8cd16e4f85cbf869f0dc124291b52
kengkeelim <limkengkee88@gmail.com>
2021-02-08 22:54:56 +0800

    Customize tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
@@ -5,7 +7,19 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      textColor: ["active"],
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const extendUnderline = {
+        ".underline": {
+          textDecoration: "underline #F87171 solid",
+          textUnderlinePosition: "under",
+        },
+      };
+      addUtilities(extendUnderline);
+    }),
+  ],
 };
