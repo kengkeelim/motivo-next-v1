ac9f98bf79623c424969ec63dbdaa8a53cf1ec52
kengkeelim <limkengkee88@gmail.com>
2021-02-08 22:57:26 +0800

    Update app and yarn file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,9 @@
 // import '../styles/globals.css';
 import "tailwindcss/tailwind.css";
-import NavBar from "../components/navBar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <NavBar />
       <Component {...pageProps} />
     </div>
   );
