dd230bd5a8bfa43103afb8a932faa6b524e5eb6b
kengkeelim <limkengkee88@gmail.com>
2021-02-26 00:03:52 +0800

    Add GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-56WNPZM" });
+  }, []);
+
   return (
     <div>
       <Component {...pageProps} />
