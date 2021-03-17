c28f02d268c884eb1b1d216f66d99e64c89c6d79
kengkeelim <limkengkee88@gmail.com>
2021-03-17 14:42:31 +0800

    Set logo size

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -53,6 +53,8 @@ function navBar(props) {
                   className="h-4 w-4 fill-current"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 612.026 612.026"
+                  width="612"
+                  height="612"
                 >
                   <g>
                     <g id="_x32__33_">
